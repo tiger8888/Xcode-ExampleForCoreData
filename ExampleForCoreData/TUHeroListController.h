9308047adaea956b8c717114e23908b08410f2a9
mac@lab538 <lixinyu@tju.edu.cn>
2014-05-25 22:26:19 +0800

    3.4 创建HeroListController 注意创建文件时先把HeroListController的父类设为为TableViewController，这样Xcode会使用一个模板来预定义一系列表格视图数据源和delegate方法。 但是！！！最后还是要让HeroListController成为ViewController的子类，并使用TableView的数据源和委托，并删除其initWithStyle方法。

---
+++ ExampleForCoreData/TUHeroListController.h
@@ -0,0 +1,13 @@
+//
+//  TUHeroListController.h
+//  ExampleForCoreData
+//
+//  Created by 3dlabuser on 14-5-25.
+//  Copyright (c) 2014年 李昕宇@天津大学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TUHeroListController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
