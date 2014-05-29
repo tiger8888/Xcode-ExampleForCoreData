682e4cfe186297f18ef45b0d043f4725e83e5b99
mac@lab538 <lixinyu@tju.edu.cn>
2014-05-29 22:07:37 +0800

    第4章 隐藏在详细视图中的“怪兽” 	4.2创建详细视图控制器 		创建sb中的一个表格视图、代码中添加一个表格视图控制器类，调整好相应的表格参数，注释掉表格视图的代理。

---
+++ ExampleForCoreData/TUHeroDetailController.h
@@ -0,0 +1,13 @@
+//
+//  TUHeroDetailController.h
+//  ExampleForCoreData
+//
+//  Created by 3dlabuser on 14-5-29.
+//  Copyright (c) 2014年 李昕宇@天津大学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TUHeroDetailController : UITableViewController
+
+@end
