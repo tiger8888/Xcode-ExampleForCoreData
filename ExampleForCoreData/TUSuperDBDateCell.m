5597443c46e1c55ef6e520fc5f9bf2bc84c4f800
mac@lab538 <lixinyu@tju.edu.cn>
2014-06-01 10:22:51 +0800

    4.13专门的输入视图(有static变量用法) 4.13.1DatePicker SuperDBEditCell子类 这个例子中有static变量的声明和用法

--- ExampleForCoreData/TUSuperDBDateCell.m
+++ ExampleForCoreData/TUSuperDBDateCell.m
@@ -8,8 +8,32 @@
 
 #import "TUSuperDBDateCell.h"
 
+static NSDateFormatter *__dateFormatter = nil;
+
+@interface TUSuperDBDateCell ()
+@property (strong, nonatomic) UIDatePicker *datePicker;
+- (IBAction)datePickerChanged:(id)sender;
+@end
+
 @implementation TUSuperDBDateCell
 
++ (void)initialize {
+    __dateFormatter = [[NSDateFormatter alloc] init];
+    [__dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+}
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        self.textField.clearButtonMode = UITextFieldViewModeNever;
+        self.datePicker = [[UIDatePicker alloc] initWithFrame:CGRectZero];
+        self.datePicker.datePickerMode = UIDatePickerModeDate;
+        [self.datePicker addTarget:self
+                            action:@selector(datePickerChanged:)
+                  forControlEvents:UIControlEventValueChanged];
+        self.textField.inputView = _datePicker;
+    }
+    return self;
+}
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
@@ -27,5 +51,24 @@
     // Drawing code
 }
 */
-
+#pragma mark - SuperDBEditCell overrides
+- (id)value {
+    if (nil == self.textField.text || 0 == [self.textField.text length]) {
+        return nil;
+    }
+    return self.datePicker.date;
+}
+- (void)setValue:(id)value {
+    if (value != nil && [value isKindOfClass:[NSDate class]]) {
+        [self.datePicker setDate:value];
+        self.textField.text = [__dateFormatter stringFromDate:value];
+    } else {
+        self.textField.text = nil;
+    }
+}
+#pragma mark - (Private) Instance Methods
+- (IBAction)datePickerChanged:(id)sender {
+    NSDate *date = [self.datePicker date];
+    self.textField.text = [__dateFormatter stringFromDate:date];
+}
 @end
