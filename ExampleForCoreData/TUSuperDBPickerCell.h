df852638d5b32f42a8731effdd1298a989146e20
mac@lab538 <lixinyu@tju.edu.cn>
2014-06-01 11:38:15 +0800

    4.13.3实现一个选择器 (用于选择性别) 新建SuperDBPickerCell

---
+++ ExampleForCoreData/TUSuperDBPickerCell.h
@@ -0,0 +1,13 @@
+//
+//  TUSuperDBPickerCell.h
+//  ExampleForCoreData
+//
+//  Created by 3dlabuser on 14-6-1.
+//  Copyright (c) 2014年 李昕宇@天津大学. All rights reserved.
+//
+
+#import "TUSuperDBEditCell.h"
+
+@interface TUSuperDBPickerCell : TUSuperDBEditCell
+
+@end
---
+++ ExampleForCoreData/TUSuperDBPickerCell.m
@@ -0,0 +1,31 @@
+//
+//  TUSuperDBPickerCell.m
+//  ExampleForCoreData
+//
+//  Created by 3dlabuser on 14-6-1.
+//  Copyright (c) 2014年 李昕宇@天津大学. All rights reserved.
+//
+
+#import "TUSuperDBPickerCell.h"
+
+@implementation TUSuperDBPickerCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
