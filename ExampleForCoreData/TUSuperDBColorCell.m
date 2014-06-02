4430c354456f9d35c26aa61777672c313694afd4
mac@lab538 <lixinyu@tju.edu.cn>
2014-06-02 20:09:45 +0800

    6.8.2自定义Color表格视图单元格 没有一种机制来在表格式图单元格中显示颜色，这里也不会构建一个复杂的表格视图单元格，而是使用一个简单的解决方案：用特殊的Unicode字符填充文本域，使它看起来是一个矩形块，再设置这个矩形块的字体颜色。 首先创建ColorCell。

---
+++ ExampleForCoreData/TUSuperDBColorCell.m
@@ -0,0 +1,31 @@
+//
+//  TUSuperDBColorCell.m
+//  ExampleForCoreData
+//
+//  Created by 3dlabuser on 14-6-2.
+//  Copyright (c) 2014年 李昕宇@天津大学. All rights reserved.
+//
+
+#import "TUSuperDBColorCell.h"
+
+@implementation TUSuperDBColorCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
