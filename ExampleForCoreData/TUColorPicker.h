6509b5dfc66a5b4bdec73f6f9dd355361af75f64
mac@lab538 <lixinyu@tju.edu.cn>
2014-06-02 17:28:59 +0800

    6.8创建Color表格视图单元格  6.8.1自定义颜色编辑器 新建ColorPicker类，继承自UIControl。 UIControl是诸如按钮、滑块这样的控件对象的基类。

---
+++ ExampleForCoreData/TUColorPicker.h
@@ -0,0 +1,13 @@
+//
+//  TUColorPicker.h
+//  ExampleForCoreData
+//
+//  Created by 3dlabuser on 14-6-2.
+//  Copyright (c) 2014年 李昕宇@天津大学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TUColorPicker : UIControl
+
+@end
